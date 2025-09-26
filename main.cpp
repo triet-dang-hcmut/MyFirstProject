eb53012541749ed6c29344894479a76f16636967
Triet Dang <triet.dangminh@hcmut.edu.vn>
2025-09-26 17:12:15 +0700

    Third commit: Add a calculation 15 divides by 2

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 using namespace std;
 int main()
 {
-    cout << "Hello, world! (with Copilot)" << endl;
+    cout << "Hello, world! (with Copilot)" << endl; 
+    cout << 15%2;
     return 0;
 }
+
+
