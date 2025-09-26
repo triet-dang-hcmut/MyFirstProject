1b396aea6bddc43759fc0ec1ecddab2c0bcc7807
Triet Dang <triet.dangminh@hcmut.edu.vn>
2025-09-26 17:42:46 +0700

    Fourth commit: try different divisions

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main()
 {
     cout << "Hello, world! (with Copilot)" << endl; 
-    cout << 15%2;
+    cout << 15%2 << endl;
+    cout << 15.0/2.0 << endl;
+    cout << 15/2 << endl;
     return 0;
 }
-
-
 
